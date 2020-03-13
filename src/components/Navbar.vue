0e2c4a31bf4074467b182494e082ad6cd6db4240
CaioFML <caio.felipemotta@hotmail.com>
2020-03-13 17:10:34 -0300

    correcao

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,17 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <h1 class="has-text-white title">OnebitTinder</h1>
-      </a>
-    </div>
-  </nav>
+  <div id="app">
+    <Navbar />
+    <router-view/>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-  nav {
-    background-color: #FF6666;
+<script>
+
+import Navbar from "./components/Navbar";
+
+export default {
+  components: {
+    Navbar
   }
-</style>
+}
+</script>
